ad71d14e90ba108ceb07151980348a782d14273a
Cristian Mateos López <mateos.c19@gmail.com>
2021-01-18 18:39:52 +0100

    Initial commit

---
+++ product-documentation-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @since             1.0.0
+ * @package           Product_Documentation_Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Product Documentation Plugin
+ * Description:       This plugin attach documentation to products.
+ * Version:           1.0.0
+ * Author:            Cristian Mateos López
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       product-documentation-plugin
+ * Domain Path:       /languages
+ */
